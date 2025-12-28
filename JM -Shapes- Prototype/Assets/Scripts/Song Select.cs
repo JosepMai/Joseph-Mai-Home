aa2b2aaee650a5572f0e811f1ca23686b90838a2
JosepMai <JosephMai0216@gmail.com>
2025-12-28 00:19:40 -0500

    working on menu screen
    r2tq4gwy

--- JM -Shapes- Prototype/Assets/Scripts/Song Select.cs
+++ JM -Shapes- Prototype/Assets/Scripts/Song Select.cs
@@ -8,6 +8,15 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScenes : MonoBehaviour
 {
+    public GameObject CurrentImage;
+    public GameObject CanonImage;
+    public GameObject DevilImage;
+    public GameObject CDImage;
+    public GameObject WindBreakerImage;
+    public GameObject HUGSImage;
+    public GameObject ThousandNightsImage;
+    public GameObject FlameWallImage;
+    public GameObject Coverup;
     public int ClickCounter = 0;
     public bool FirstClick = true;
     public bool SecondClick = false;
@@ -23,6 +32,10 @@ public class ChangeScenes : MonoBehaviour
     {
         if (HighlightedLevel != "Canon")
         {
+            CanonImage.SetActive(true);
+            CurrentImage.SetActive(false);
+            CurrentImage = CanonImage;
+            Coverup.SetActive(false);
             HighlightedLevel = "Canon";
             ClickCounter = 0;
         }
@@ -47,6 +60,10 @@ public class ChangeScenes : MonoBehaviour
     {
         if (HighlightedLevel != "Devil")
         {
+            DevilImage.SetActive(true);
+            CurrentImage.SetActive(false);
+            CurrentImage = DevilImage;
+            Coverup.SetActive(false);
             HighlightedLevel = "Devil";
             ClickCounter = 0;
         }
@@ -71,6 +88,10 @@ public class ChangeScenes : MonoBehaviour
     {
         if (HighlightedLevel != "CD")
         {
+            CDImage.SetActive(true);
+            CurrentImage.SetActive(false);
+            CurrentImage = CDImage;
+            Coverup.SetActive(false);
             HighlightedLevel = "CD";
             ClickCounter = 0;
         }
@@ -95,6 +116,10 @@ public class ChangeScenes : MonoBehaviour
     {
         if (HighlightedLevel != "WindBreaker")
         {
+            WindBreakerImage.SetActive(true);
+            CurrentImage.SetActive(false);
+            CurrentImage = WindBreakerImage;
+            Coverup.SetActive(false);
             HighlightedLevel = "WindBreaker";
             ClickCounter = 0;
         }
@@ -111,6 +136,90 @@ public class ChangeScenes : MonoBehaviour
             SceneManager.LoadScene("Wind Breaker");
         }
         else if (ClickCounter == 2 && HighlightedLevel != "WindBreaker")
+        {
+            ClickCounter = 0;
+        }
+    }
+    public void HUGS()
+    {
+        if (HighlightedLevel != "HUGS")
+        {
+            HUGSImage.SetActive(true);
+            CurrentImage.SetActive(false);
+            CurrentImage = HUGSImage;
+            Coverup.SetActive(false);
+            HighlightedLevel = "HUGS";
+            ClickCounter = 0;
+        }
+
+        if (HighlightedLevel == "HUGS")
+        {
+            ClickCounter += 1;
+        }
+    }
+    public void LoadHUGS()
+    {
+        if (ClickCounter == 2 && HighlightedLevel == "HUGS")
+        {
+            SceneManager.LoadScene("Gachiakuta");
+        }
+        else if (ClickCounter == 2 && HighlightedLevel != "HUGS")
+        {
+            ClickCounter = 0;
+        }
+    }
+    public void ThousandNights()
+    {
+        if (HighlightedLevel != "ThousandNights")
+        {
+            ThousandNightsImage.SetActive(true);
+            CurrentImage.SetActive(false);
+            CurrentImage = ThousandNightsImage;
+            Coverup.SetActive(false);
+            HighlightedLevel = "ThousandNights";
+            ClickCounter = 0;
+        }
+
+        if (HighlightedLevel == "ThousandNights")
+        {
+            ClickCounter += 1;
+        }
+    }
+    public void LoadThousandNights()
+    {
+        if (ClickCounter == 2 && HighlightedLevel == "ThousandNights")
+        {
+            SceneManager.LoadScene("Hilcrhyme");
+        }
+        else if (ClickCounter == 2 && HighlightedLevel != "ThousandNights")
+        {
+            ClickCounter = 0;
+        }
+    }
+    public void Flamewall()
+    {
+        if (HighlightedLevel != "Flamewall")
+        {
+            FlameWallImage.SetActive(true);
+            CurrentImage.SetActive(false);
+            CurrentImage = FlameWallImage;
+            Coverup.SetActive(false);
+            HighlightedLevel = "Flamewall";
+            ClickCounter = 0;
+        }
+
+        if (HighlightedLevel == "Flamewall")
+        {
+            ClickCounter += 1;
+        }
+    }
+    public void LoadFlamewall()
+    {
+        if (ClickCounter == 2 && HighlightedLevel == "Flamewall")
+        {
+            SceneManager.LoadScene("Flamewall");
+        }
+        else if (ClickCounter == 2 && HighlightedLevel != "Flamewall")
         {
             ClickCounter = 0;
         }
